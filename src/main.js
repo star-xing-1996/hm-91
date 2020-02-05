4c5f90cf7ca98ec6a57492ee746ccbd9c3737756
WangXing <WangXing@example.com>
2020-02-05 20:01:38 +0800

    调整结构并配置尺寸大小

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入vant组件
+import 'vant/lib/index.css' // 引入vant组件css样式
+import 'amfe-flexible'// 引入自动适配
+Vue.use(Vant) // 全局注册vant
 
 Vue.config.productionTip = false
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>公共布局</div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
