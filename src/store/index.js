aaa09f3c2f6074f24f340a181508fad4e2efc02d
WangXing <WangXing@example.com>
2020-02-05 22:54:07 +0800

    route里设置更新和清空数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 放置公共数据
   state: {
+    user: auth.getUser() // 获取缓存中的数据
   },
   mutations: {
+    updateUser (state, payload) {
+      state.user = payload.user// 更新缓存中的数据
+      auth.setUser(payload.user)// 将缓存中的数据放到本地存储中
+    },
+    clearUser (state) {
+      state.user = {}
+      auth.delUser()
+    }
   },
   actions: {
   },
