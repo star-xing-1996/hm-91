4e89c3eaae347828ccd48f551aed4c86137d345b
WangXing <WangXing@example.com>
2020-02-06 21:46:16 +0800

    完成登录布局及配置vant主题颜色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission' //
 import store from './store'
 import Vant from 'vant' // 引入vant组件
-import 'vant/lib/index.css' // 引入vant组件css样式
+import 'vant/lib/index.less' // 引入vant组件css样式
 import '@/styles/index.less'// 引入全局样式
 import 'amfe-flexible'// 引入自动适配
 Vue.use(Vant) // 全局注册vant
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
     <van-nav-bar left-arrow @click-left="$router.back()" title="登录" ></van-nav-bar>
+  <van-cell-group>
+    <van-field label="手机号" placeholder="请输入手机号"></van-field>
+    <van-field label="验证码" placeholder="请输入验证码">
+      <van-button slot="button" size="small" type="primary">获取验证码</van-button>
+    </van-field>
+</van-cell-group>
+<div class="btn-box">
+<van-button type="info" block size="small" round>登录</van-button>
+</div>
   </div>
 </template>
 
@@ -11,4 +20,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn-box{
+  padding: 20px;
+}
 </style>
