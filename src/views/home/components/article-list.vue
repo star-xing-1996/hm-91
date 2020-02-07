3d27b5f8b67a150891abd2001d9cf5ec57497332
WangXing <WangXing@example.com>
2020-02-07 21:19:24 +0800

    完成下拉树刷新（虚拟数据）

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,10 +1,12 @@
 <template>
        <!-- 设置滚动条，为做阅读记忆留下伏笔 -->
     <div class="scroll-wrapper">
+        <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
         <van-list v-model="upLoading" :finished="finished"  finished-text="没有更多了"
   @load="onLoad">
         <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
         </van-list>
+        </van-pull-refresh>
   </div>
 </template>
 
@@ -13,8 +15,10 @@ export default {
   name: 'article-list',
   data () {
     return {
-      upLoading: false, // 是否加载数据
-      finished: false, // 是否完成加载
+      downLoading: false, // 下拉是否加载数据
+      refreshSuccessText: '更新成功', //  文本
+      upLoading: false, // 上拉是否加载数据
+      finished: false, // 上拉是否完成加载
       articles: []// 定义文章
     }
   },
@@ -28,6 +32,14 @@ export default {
         } else {
           this.finished = true// 停止追加
         }
+      }, 1000)
+    },
+    onRefresh () {
+      setTimeout(() => {
+        let arr = Array.from(Array(10), (value, index) => ('追加' + (index + 1)))
+        this.articles.unshift(...arr)
+        this.downLoading = false// 关闭状态
+        this.refreshSuccessText = `更新了${arr.length}条数据`
       }, 1000)
     }
   }
